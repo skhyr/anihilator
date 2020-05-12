45778082cd9ec3a0be5507088630d592e7d1b54c
skhyr <piotrmal2002@gmail.com>
2020-05-12 13:03:29 +0200

    update info

--- popup.js
+++ popup.js
@@ -1,8 +1,17 @@
+var manifest = chrome.runtime.getManifest();
 
 const setup = () =>{
     chrome.storage.local.get('state', function({state}){
         document.querySelector('input').checked = state;
     });
+
+    fetch('https://raw.githubusercontent.com/skhyr/anihilator/master/version')
+    .then(response => response.json())
+    .then(data => {
+        if(data === Number(manifest.version)) document.querySelector('#update').innerText = '' 
+        else document.querySelector('#update').innerText = 'update available'; 
+    });
+
 };
 setup();
   
