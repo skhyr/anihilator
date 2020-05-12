0bc689b22218cc91ba3a6f03f60a4bf360f8295c
skhyr <piotrmal2002@gmail.com>
2020-05-12 11:22:49 +0200

    v2.0

--- pageScript.js
+++ pageScript.js
@@ -1,6 +1,8 @@
-    console.log('bang');
+    //console.log('bang');
     try{
-        blurSpy.stop();
+
+        blurSpy.onBlur = ()=>{}
+        
         window.postMessage({action: 'GOT_DUCK', payload: true}, '*');
     }catch(error){
         window.postMessage({action: 'GOT_DUCK', payload: false}, '*');
